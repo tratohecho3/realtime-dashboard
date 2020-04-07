ea982280211387ddffac93135b0f6eeb2dec8230
Cesar <tratohecho3@gmail.com>
2020-04-07 18:12:21 -0400

    fix deployment heroku

--- index.ts
+++ index.ts
@@ -1,10 +1,17 @@
 import Server from "./classes/server";
 import router from "./routes/router";
 import cors from "cors";
+import express from "express";
+import path = require("path");
 const server = Server.instance;
 
 // CORS
 server.app.use(cors({ origin: true, credentials: true }));
+
+if (process.env.NODE_ENV === "production") {
+  server.app.use(express.static(path.join(process.cwd(), "/website/dist")));
+}
+
 //ROUTES
 server.app.use("/", router);
 
